b9f333906ab369fde78351f8eaf61f62393c94ff
EslamKamel89 <elsamkamelforex@gmail.com>
2026-01-20 04:25:08 +0200

    feat(websocket): add health check endpoint and echo loop for websocket messages

--- app/main.py
+++ app/main.py
@@ -3,6 +3,16 @@ from fastapi import FastAPI, WebSocket
 app = FastAPI(title="Phase 2 websocket course")
 
 
+@app.get("/")
+async def home():
+    return {"status": "ok"}
+
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+    while True:
+        print("start while loop")
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Echo: {data}")
+        print("end while loop")
