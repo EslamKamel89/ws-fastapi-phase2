a58624b90db24c5bc479039ed736968d7998b0b9
EslamKamel89 <elsamkamelforex@gmail.com>
2026-01-22 15:07:35 +0200

    feat(websocket): validate token before accepting connection

--- app/main.py
+++ app/main.py
@@ -10,6 +10,12 @@ async def home():
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    secret = websocket.query_params.get("token")
+    print("secret", secret)
+    if secret != "password":
+        print("invalid token")
+        await websocket.close(1008, reason="invalid token")
+        return
     await websocket.accept()
     while True:
         print("start while loop")
